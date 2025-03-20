4772d239a68fb29b1cc9ce4d86ad64e567d29fbc
elvisbaldow <elvisbaldow@gmail.com>
2025-03-20 18:08:37 -0300

    button edit&delete

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function adicionarTarefa() {
           mensagem.style.color = "#A34743"; 
           mensagem.textContent = erro;
         } else {
-          mensagem.style.color = "#28A745"; 
+          mensagem.style.color = "#026120";
           mensagem.textContent = positivo;
           
           tarefas.push(tarefa);
@@ -27,10 +27,59 @@ function adicionarTarefa() {
     const listaTarefas = document.getElementById("listaTarefas");
     listaTarefas.innerHTML = ""; //limpa a lista de tarefas para não duplicar as tarefas.
 
-    let i = 0;
-    for (i; i < tarefas.length; i++) {
+    
+    for (let i = 0; i < tarefas.length; i++) {
       let novaTarefa = document.createElement("li");
       novaTarefa.textContent = tarefas[i];
+
+      // botões adcionados na LI (editar e remover)
+
+      let botaoEditar = document.createElement("button");
+      botaoEditar.className = "editar";
+      novaTarefa.appendChild(botaoEditar);
+      botaoEditar.textContent = "Editar";
+      
+      // funçao editar tarefa
+      botaoEditar.onclick = () => editarTarefa(i);
+
+
+      let botaoExcluir = document.createElement("button");
+      botaoExcluir.className = "remover";
+      novaTarefa.appendChild(botaoExcluir);
+      botaoExcluir.textContent = "Excluir";
+      
+      // funçao remover tarefa
+      botaoExcluir.onclick = () => removerTarefa(i);
+
+
+
       listaTarefas.appendChild(novaTarefa);
     }
+  }
+
+  function removerTarefa(i){
+      tarefas.splice(i, 1); // (indice, n0 indicando qtos itens serão excluidos)
+      renderizarTarefas();
+      if (tarefas.length === 0) {
+        const mensagem = document.getElementById("mensagem"); 
+        mensagem.style.color = "#A34743"; 
+        mensagem.textContent = "Lista de tarefas limpa!";
+
+      }
+  }
+
+  function editarTarefa(i){
+    let tarefaEditada = prompt("Edite a tarefa");
+    if (tarefaEditada.trim() !== "") {
+      tarefas[i] = tarefaEditada;
+      renderizarTarefas();
+    }
+}
+
+  function limparTarefa(){
+    tarefas.length = 0;
+    renderizarTarefas();
+    const mensagem = document.getElementById("mensagem"); 
+    mensagem.style.color = "#A34743"; 
+    mensagem.textContent = "Lista de tarefas limpa com Sucesso!";
   }
